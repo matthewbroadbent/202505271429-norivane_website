52da9093663d0f3cc450f953d6f08d93e0959074
matthewbroadbent <matthew@norivane.com>
2025-05-30 21:27:19 +0100

    202505302127 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import ServicesPage from './pages/ServicesPage'
-import ProcessPage from './pages/ProcessPage'
-import ContactPage from './pages/ContactPage'
-import BookingPage from './pages/BookingPage'
-import SettingsPage from './pages/Settings'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Booking from './pages/Booking'
+import Settings from './pages/Settings'
+import NotFound from './pages/NotFound'
 
 function App() {
+  const location = useLocation()
+  
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+  
+  // Log current route for debugging
+  useEffect(() => {
+    console.log('Current route:', location.pathname)
+  }, [location.pathname])
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/process" element={<ProcessPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/booking" element={<BookingPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
